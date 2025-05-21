b44e80acc50df6b95c3e4bb9244d502e1d59d3a1
oignon <asappy1019@gmail.com>
2025-05-22 01:34:54 +0900

    Delayed prism.js declaration after script.js in piano.js article. update piano.js article.

--- pages/piano-js-lib/script.js
+++ pages/piano-js-lib/script.js
@@ -2,6 +2,23 @@ fetch('https://oignonasappy.github.io/monologue/pages/piano-js-lib/piano.js')
     .then(responce => responce.text())
     .then(text => {
         document.getElementById('piano-js-src').textContent = text;
+
+        const prismCore = document.createElement('script');
+        prismCore.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/components/prism-core.min.js';
+        prismCore.defer = true;
+        document.head.appendChild(prismCore);
+        const prismAutoloader = document.createElement('script');
+        prismAutoloader.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/autoloader/prism-autoloader.min.js';
+        prismAutoloader.defer = true;
+        document.head.appendChild(prismAutoloader);
+        const prismWhitespace = document.createElement('script');
+        prismWhitespace.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js';
+        prismWhitespace.defer = true;
+        document.head.appendChild(prismWhitespace);
+        const prismLineNumber = document.createElement('script');
+        prismLineNumber.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/line-numbers/prism-line-numbers.min.js';
+        prismLineNumber.defer = true;
+        document.head.appendChild(prismLineNumber);
     })
     .catch(e => {
         document.getElementById('piano-js-src').textContent = "/* うまくよみこめんかった */";
