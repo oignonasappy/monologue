1494d4a27c1399182b8dda2df9f0af4ed58e65dd
oignon <asappy1019@gmail.com>
2025-05-28 01:50:44 +0900

    create article dominant7th-scales.

--- pages/piano-js-lib/script.js
+++ pages/piano-js-lib/script.js
@@ -1,26 +1,27 @@
-fetch('https://oignonasappy.github.io/pianojs/v1.0.1/piano.js')
+fetch('https://oignonasappy.github.io/pianojs/v1.0.2/piano.js')
     .then(responce => {
         return responce.text();
     })
     .then(text => {
         document.getElementById('piano-js-src').textContent = text;
 
+        const prismCore = document.createElement('script');
+        prismCore.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/components/prism-core.min.js';
+        prismCore.defer = true;
+        const prismAutoloader = document.createElement('script');
+        prismAutoloader.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/autoloader/prism-autoloader.min.js';
+        prismAutoloader.defer = true;
+        const prismWhitespace = document.createElement('script');
+        prismWhitespace.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js';
+        prismWhitespace.defer = true;
+        const prismLineNumber = document.createElement('script');
+        prismLineNumber.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/line-numbers/prism-line-numbers.min.js';
+        prismLineNumber.defer = true;
+
         setTimeout(() => {
-            const prismCore = document.createElement('script');
-            prismCore.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/components/prism-core.min.js';
-            prismCore.defer = true;
             document.head.appendChild(prismCore);
-            const prismAutoloader = document.createElement('script');
-            prismAutoloader.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/autoloader/prism-autoloader.min.js';
-            prismAutoloader.defer = true;
             document.head.appendChild(prismAutoloader);
-            const prismWhitespace = document.createElement('script');
-            prismWhitespace.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js';
-            prismWhitespace.defer = true;
             document.head.appendChild(prismWhitespace);
-            const prismLineNumber = document.createElement('script');
-            prismLineNumber.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/line-numbers/prism-line-numbers.min.js';
-            prismLineNumber.defer = true;
             document.head.appendChild(prismLineNumber);
         }, 1000);
     })
