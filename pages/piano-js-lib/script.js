49642d93201a77f4cfdf403fd3276e0708ed50ff
oignon <asappy1019@gmail.com>
2025-06-04 22:08:40 +0900

    update piano.js version

--- pages/piano-js-lib/script.js
+++ pages/piano-js-lib/script.js
@@ -1,4 +1,6 @@
-fetch('https://oignonasappy.github.io/pianojs/v1.1.0/piano.js')
+import { contentReady } from '../../general/js/common.js';
+
+fetch('https://oignonasappy.github.io/pianojs/v1.1.2/piano.js')
     .then(responce => {
         return responce.text();
     })
@@ -7,23 +9,23 @@ fetch('https://oignonasappy.github.io/pianojs/v1.1.0/piano.js')
 
         const prismCore = document.createElement('script');
         prismCore.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/components/prism-core.min.js';
-        prismCore.defer = true;
+        //prismCore.defer = true;
         const prismAutoloader = document.createElement('script');
         prismAutoloader.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/autoloader/prism-autoloader.min.js';
-        prismAutoloader.defer = true;
+        //prismAutoloader.defer = true;
         const prismWhitespace = document.createElement('script');
         prismWhitespace.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js';
-        prismWhitespace.defer = true;
+        //prismWhitespace.defer = true;
         const prismLineNumber = document.createElement('script');
         prismLineNumber.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.30.0/plugins/line-numbers/prism-line-numbers.min.js';
-        prismLineNumber.defer = true;
+        //prismLineNumber.defer = true;
 
-        setTimeout(() => {
-            document.head.appendChild(prismCore);
-            document.head.appendChild(prismAutoloader);
-            document.head.appendChild(prismWhitespace);
-            document.head.appendChild(prismLineNumber);
-        }, 1000);
+        contentReady.then(() => {
+            document.body.appendChild(prismCore);
+            document.body.appendChild(prismAutoloader);
+            document.body.appendChild(prismWhitespace);
+            document.body.appendChild(prismLineNumber);
+        });
     })
     .catch(e => {
         document.getElementById('piano-js-src').textContent = "/* うまくよみこめんかった */";
