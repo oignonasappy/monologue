671a086d00692e547b53b5ebb0b8912b250e8d1d
oignon <asappy1019@gmail.com>
2025-06-20 18:48:39 +0900

    update piano-js-lib piano.js version

--- pages/piano-js-lib/script.js
+++ pages/piano-js-lib/script.js
@@ -1,6 +1,6 @@
 import { contentReady } from '../../general/js/common.js';
 
-fetch('https://oignonasappy.github.io/pianojs/v1.1.2/piano.js')
+fetch('https://oignonasappy.github.io/pianojs/v1.2.0/piano.js')
     .then(responce => {
         return responce.text();
     })
