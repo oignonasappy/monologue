518b10c88fc3536e68dadb05254218978d1e0a1c
oignon <asappy1019@gmail.com>
2025-07-06 01:44:44 +0900

    24平均律の周波数テキストを追加

--- pages/stacked-chord-temperament-comparison/script.js
+++ pages/stacked-chord-temperament-comparison/script.js
@@ -3,6 +3,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 3, 2), '#p5-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 7, 12), '#p5-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 10, 17), '#p5-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 14, 24), '#p5-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 18, 31), '#p5-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 24, 41), '#p5-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 31, 53), '#p5-box-53');
@@ -11,6 +12,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 4, 3), '#p4-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 5, 12), '#p4-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 7, 17), '#p4-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 10, 24), '#p4-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 13, 31), '#p4-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 17, 41), '#p4-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 22, 53), '#p4-box-53');
@@ -19,6 +21,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 5, 4), '#maj3-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 4, 12), '#maj3-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 5, 17), '#maj3-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 8, 24), '#maj3-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 10, 31), '#maj3-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 13, 41), '#maj3-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 17, 53), '#maj3-box-53');
@@ -27,6 +30,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 6, 5), '#min3-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 3, 12), '#min3-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 4, 17), '#min3-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 6, 24), '#min3-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 8, 31), '#min3-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 11, 41), '#min3-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 14, 53), '#min3-box-53');
@@ -35,6 +39,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 7, 4), '#n7-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 10, 12), '#n7-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 14, 17), '#n7-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 19, 24), '#n7-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 25, 31), '#n7-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 33, 41), '#n7-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 43, 53), '#n7-box-53');
@@ -43,6 +48,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 7, 6), '#nmin3-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 3, 12), '#nmin3-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 4, 17), '#nmin3-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 5, 24), '#nmin3-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 7, 31), '#nmin3-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 9, 41), '#nmin3-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 12, 53), '#nmin3-box-53');
@@ -51,6 +57,7 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 11, 8), '#n11-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 6, 12), '#n11-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 8, 17), '#n11-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 11, 24), '#n11-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 14, 31), '#n11-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 19, 41), '#n11-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 24, 53), '#n11-box-53');
@@ -59,14 +66,16 @@
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 11, 9), '#n3-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 3, 12), '#n3-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 5, 17), '#n3-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 7, 24), '#n3-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 9, 31), '#n3-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 12, 41), '#n3-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 15, 53), '#n3-box-53');
-    
+
     /* 十三限界の短六度 */
     createFrequencyTexts(generateJustIntonationFrequency(10, 55, 13, 8), '#nmin6-box-ji');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 8, 12), '#nmin6-box-12');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 12, 17), '#nmin6-box-17');
+    createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 17, 24), '#nmin6-box-24');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 22, 31), '#nmin6-box-31');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 29, 41), '#nmin6-box-41');
     createFrequencyTexts(generateEqualTemperamentFrequency(10, 55, 37, 53), '#nmin6-box-53');
