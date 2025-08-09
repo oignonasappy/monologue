0b3cd1c5f59800be684fce41ab4fd08107bc4df9
oignon <asappy1019@gmail.com>
2025-08-09 14:19:46 +0900

    インデント幅を変更するツールを追加し、関連するHTML、CSS、JavaScriptファイルを作成しました。

---
+++ pages/change-indent-depth/script.js
@@ -0,0 +1,51 @@
+const input = document.querySelector('#input');
+const depth = document.querySelector('#depth');
+
+(() => {
+
+    document.querySelector('#process').addEventListener('click', update);
+
+    document.querySelector('#copy').addEventListener('click', () => {
+        navigator.clipboard.writeText(input.value);
+    });
+
+    document.querySelector('#clear').addEventListener('click', () => {
+        input.value = "";
+    });
+
+})();
+
+function update() {
+    /**
+     * @type {string}
+     */
+    const text = input.value;
+    /**
+     * @type {string[]}
+     */
+    const textArray = text.split('\n');
+
+    let currentDepth = 0;
+    const targetDepth = parseInt(depth.value);
+
+    for (let i = 0; i < textArray.length; i++) {
+        if (textArray[i].startsWith(" ")) {
+            currentDepth = textArray[i].length - textArray[i].trimStart().length;
+            break;
+        }
+    }
+
+    let returnString = "";
+    for (let i = 0; i < textArray.length; i++) {
+        returnString += ' '
+            .repeat(
+                Math.floor(
+                    (textArray[i].length - textArray[i].trimStart().length)
+                    / currentDepth
+                ) * targetDepth
+            ) + textArray[i].trimStart()
+            + '\n';
+    }
+
+    input.value = returnString;
+}
