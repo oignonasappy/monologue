8f504f549e8c64e9cabb8f07e6cf57277980fac6
oignon <asappy1019@gmail.com>
2025-09-02 01:21:57 +0900

    ボタンのスタイルを追加し、パズルのレイアウトを改善しました

--- pages/lightsout-3d/script.js
+++ pages/lightsout-3d/script.js
@@ -1,3 +1,18 @@
 document.getElementById('subview-size').addEventListener('change', () => {
+    console.log("size changed");
+    
     /* subviewのサイズ変更 */
 });
+
+document.getElementById('button-top').addEventListener('click', () => {
+    console.log("top");
+})
+document.getElementById('button-right').addEventListener('click', () => {
+    console.log("right");
+})
+document.getElementById('button-bottom').addEventListener('click', () => {
+    console.log("bottom");
+})
+document.getElementById('button-left').addEventListener('click', () => {
+    console.log("left");
+})
